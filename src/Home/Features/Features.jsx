7f644cd59d939a9d2ace744b1fd10c6687f3ad3d
mardon1dev <dusbekovmardonbek5430@gmail.com>
2023-03-20 01:36:26 +0500

    ok?

--- src/Home/Features/Features.jsx
+++ src/Home/Features/Features.jsx
@@ -18,26 +18,24 @@ const Features = () => {
         data ? (
             <Row className='pb-5 pt-5 features'>{
                 data.articles.slice(3,6).map((el)=>(
-
-                    
-                        <Col md={4} className="gy-5">
+                    <Col md={4} className="gy-5">
                         <Card style={{ width: '100%' }} className="features__all border-0">
-                      <Card.Img variant="top" src={el.urlToImage} style={{height: "350px"}} className="features__img"/>
-                      <Card.Body style={{width: "70%"}} className="features__card">
-                        <Card.Title className='features__card--title'>{el.title.split(" ").slice(0,4).join(" ")}</Card.Title>
-                        <Card.Text className='features__card--text px-5 px-md-0'>
-                          {el.description.split(" ").slice(0,10).join(" ")}
-                        </Card.Text>
-                        <a href={el.url} className='d-block text-center features__card--span'>Read more</a>
-                      </Card.Body>
-                    </Card>
-                        </Col>       
-                )
-                )
-            }</Row>
-          ) : (
+                            <Card.Img variant="top" src={el.urlToImage} style={{height: "350px"}} className="features__img"/>
+                            <Card.Body style={{width: "70%"}} className="features__card">
+                                <Card.Title className='features__card--title'>{el.title.split(" ").slice(0,4).join(" ")}</Card.Title>
+                                <Card.Text className='features__card--text px-5 px-md-0'>
+                                    {el.description.split(" ").slice(0,10).join(" ")}
+                                </Card.Text>
+                                <a href={el.url} className='d-block text-center features__card--span'>Read more</a>
+                            </Card.Body>
+                        </Card>
+                    </Col>       
+                    )
+                )}
+            </Row>
+        ) : (
             <div className='text-center'>Loading...</div>
-          )}
+        )}
     </div>
         </div>
   )
