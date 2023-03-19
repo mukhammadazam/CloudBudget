0d1ad0cd09bc42a576eeecc781b5140bb113c4ab
mardon1dev <dusbekovmardonbek5430@gmail.com>
2023-03-20 04:41:01 +0500

    money is added

--- src/Home/Money/Money.jsx
+++ src/Home/Money/Money.jsx
@@ -8,29 +8,27 @@ import Windows from './windows.svg';
  
 const Money = () => {
   return (
-    <div className='money'>
-      <div className='container'>
-        <div>
-        <div className='row moneyall d-flex align-items-center justify-content-between g-5 pt-5 pb-5'>
-            <div className='col-md-6 col-12 money'>
-                <div className='money__main text-center text-md-start'>
+    <div className='moneyal'>
+      <div className='container'>        
+        <div className='row moneyall d-flex al justify-content-between g-5 pt-5 pb-5'>
+            <div className='col-md-6 col-12 money text-md-start text-center '>
+                <div className='money__main text-center text-md-start  px-md-0 px-5'>
                     <h3 className='money__main--title'>Easy to Use Cloud Budget Management Software</h3>
                     <p className='money__main--text'>Our software is made so you can access and manage your budget and expenses online at any time from any device. It provides detailed income and expense reports with graphs so you can easilly see your spending patterns and budget at a glance. Read below to find out more.</p>
-                    <div className='money__main--social'>
+                    <div className='money__main--social d-flex justify-content-center justify-content-md-start'>
                       <img src={PlayMarket} alt="Playmarket" />
                       <img src={Apple} alt="Aplle" />
                       <img src={Windows} alt="Windows" />
                     </div>
                 </div>
             </div>
-            <div className='col-md-6 col-12 money__image'>
+            <div className='col-md-6 col-12 money__image px-md-0 px-5'>
                   <div className='money__image--wrapper position-relative'>
-                    <img src={MoneyImg} alt="Money" style={{width: "100%"}}/>
-                    <img src={MoneyBg1} alt="Moneybg1" />
-                    <img src={MoneyBg2} alt="Moneybg2" />
+                    <img src={MoneyImg} alt="Money" style={{width: "90%"}}/>
+                    <img src={MoneyBg1} alt="Moneybg1"style={{width: "60%"}} className='position-absolute'/>
+                    <img src={MoneyBg2} alt="Moneybg2"style={{width: "60%"}} className='position-absolute'/>
                   </div>
             </div>
-        </div>
         </div>
       </div>
 
