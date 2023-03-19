7a35d8e3b8e510838b9035ff87fc602c9cd568c4
Mukhammad_Azam_001 <rizayevmuhammadazam9@gmail.com>
2023-03-20 00:47:34 +0300

    header finished

--- src/Home/Header.jsx
+++ src/Home/Header.jsx
@@ -1,23 +1,38 @@
-import Logo from './imges/Logo.svg';
-import './scss/main.scss';
+import Logo from "./imges/Logo.svg";
+import "./scss/main.scss";
 const headerFun = () => {
-return <>
-<header className='header'>
-  <nav>
-    <div className="container">
-      <div className="nav">
-        <a href="logo" className="nav__link"><img src={Logo} alt="logo" /></a>
-        <ul className="nav__list">
-          <li className="nav__list--item"><a href="item">О компании</a></li>
-          <li className="nav__list--item"><a href="item">Портфолио</a></li>
-          <li className="nav__list--item"><a href="item">Услуги</a></li>
-          <li className="nav__list--item"><a href="item">Контакты</a></li>
-        </ul>       
-      </div>
-    </div>
-  </nav>
-</header>
-  </>;
+  return (
+    <>
+      <header className="header">
+        <nav>
+          <div className="container">
+            <div className="nav">
+              <a href="logo" className="nav__link">
+                <img src={Logo} alt="logo" />
+              </a>
+              <ul className="nav__list">
+                <li className="nav__list--item">
+                  <a href="item">OVERVIEW</a>
+                </li>
+                <li className="nav__list--item">
+                  <a href="item">FEATURES</a>
+                </li>
+                <li className="nav__list--item">
+                  <a href="item">TECHNOLOGY</a>
+                </li>
+                <li className="nav__list--item">
+                  <a href="item">CONTACT</a>
+                </li>
+                <li className="nav__list--item">
+                  <a href="item">Sign up</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </header>
+    </>
+  );
 };
 
 export default headerFun;
