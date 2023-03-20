847e5318ba14b65a495c8ee2928cbea6a13a43a1
mardon1dev <dusbekovmardonbek5430@gmail.com>
2023-03-20 07:07:20 +0500

    ok

--- src/Home/Management/Management.jsx
+++ src/Home/Management/Management.jsx
@@ -10,7 +10,7 @@ const Management = () => {
                 <div className='management__main text-center text-md-start'>
                     <h3 className='management__main--title'>Easy to Use Cloud Budget Management Software</h3>
                     <p className='management__main--text'>Our software is made so you can access and manage your budget and expenses online at any time from any device. It provides detailed income and expense reports with graphs so you can easilly see your spending patterns and budget at a glance. Read below to find out more.</p>
-                    <a className='management__main--link' href="#">Learn More</a>
+                    <a className='management__main--link' href="#learn">Learn More</a>
                 </div>
             </div>
             <div className='col-md-6 col-12 management__image'>
