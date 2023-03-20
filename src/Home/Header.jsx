2b5c5283c9769545e2f2c3b2f45a277c6989289b
Mukhammad_Azam_001 <rizayevmuhammadazam9@gmail.com>
2023-03-20 06:08:15 +0300

    hero finished

--- src/Home/Header.jsx
+++ src/Home/Header.jsx
@@ -1,49 +1,66 @@
+import { useState } from "react";
 import Logo from "./imges/Logo.svg";
+import Burger from "./imges/burger.png";
 import "./scss/main.scss";
-const headerFun = () => {
+const HeaderFun = () => {
+  const [BtnSubmit, setbtnSubmit] = useState();
+  const Btn = () => {
+    return setbtnSubmit(!BtnSubmit);
+  };
   return (
     <>
       <header className="header">
         <nav>
           <div className="container">
-            <div className="nav pt-5  d-flex align-items-center justify-content-between">
+            <div className="nav pt-5  d-flex align-items-lg-center align-items-start justify-content-between">
               <a href="logo" className="nav__link">
                 <img src={Logo} alt="logo" />
               </a>
-              <ul className="nav__list gap-5 mb-0  d-md-flex align-items-md-center justify-content-center">
+              <ul
+                className={`nav__list ${
+                  BtnSubmit ? "d-block bg-danger p-3 radius" : "d-none"}  mb-0    d-lg-flex align-items-lg-center justify-content-center`}
+              >
                 <li className="nav__list--item p-0">
-                  <a className="text-decoration-none" href="item">
+                  <a className="text-decoration-none" href="#">
                     OVERVIEW
                   </a>
                 </li>
                 <li className="nav__list--item p-0">
-                  <a className="text-decoration-none" href="item">
+                  <a className="text-decoration-none" href="#">
                     FEATURES
                   </a>
                 </li>
                 <li className="nav__list--item p-0">
-                  <a className="text-decoration-none" href="item">
+                  <a className="text-decoration-none" href="#">
                     TECHNOLOGY
                   </a>
                 </li>
                 <li className="nav__list--item p-0">
-                  <a className="text-decoration-none" href="item">
+                  <a className="text-decoration-none" href="#">
                     CONTACT
                   </a>
                 </li>
                 <li className="nav__list--item p-0">
-                  <a className="text-decoration-none" href="item">
+                  <a className="text-decoration-none" href="#">
                     Sign up
                   </a>
                 </li>
               </ul>
               <button className="nav__btn ">LOGIN</button>
+              <a onClick={Btn} className="nav__burger" href=" #">
+                <img
+                  className="bg-transparent"
+                  src={Burger}
+                  alt="burger"
+                  style={{ width: "40px", height: "40px" }}
+                />
+              </a>
             </div>
             <div className="hero">
               <h1 className="hero__title text-center">CloudBudget</h1>
               <p className="hero__text text-center">
-              Cloud budget management for everyone. Only €69.95 a Month After a 7 Day Trial of 
-Up to €4.99
+                Cloud budget management for everyone. Only €69.95 a Month After
+                a 7 Day Trial of Up to €4.99
               </p>
               <button className="hero__btn d-block">Sign Up</button>
             </div>
@@ -54,4 +71,4 @@ Up to €4.99
   );
 };
 
-export default headerFun;
+export default HeaderFun;
